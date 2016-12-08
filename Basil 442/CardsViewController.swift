798c1e929e5641babb8de75287e5cbff49536f7f
Sophie Zhao <syzhao@andrew.cmu.edu>
2016-12-08 07:20:39 -0500

    slight cards changes

--- Basil 442/CardsViewController.swift
+++ Basil 442/CardsViewController.swift
@@ -97,7 +97,7 @@ class CardsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let image = detailsInfo["imageURL"] as! String
         let servingsInt = detailsInfo["servings"] as! Int
         let servings = String(servingsInt)
-                
+        
         // Get ingredients
         let ingredientInfo:Dictionary<String, AnyObject> = recipeInstance.getIngredients(id) as! Dictionary<String, AnyObject>
         let ingredientsList:Array<String> = (ingredientInfo["ingredients"] as! Array<String>)
