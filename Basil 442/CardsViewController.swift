e66c9462b2a5b97384df7396d2be5f3cc23c7c44
Sophie Zhao <syzhao@andrew.cmu.edu>
2016-12-08 06:18:47 -0500

    attempted alert, commented out

--- Basil 442/CardsViewController.swift
+++ Basil 442/CardsViewController.swift
@@ -38,12 +38,6 @@ class CardsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     // MARK: - Table View
     
-//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//        
-//    }
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return allRecipes.count
@@ -58,29 +52,30 @@ class CardsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         cell.prepTime?.text = String(prepTime)
         return cell
     }
-    
-    func alertNoInstructions(name: String) {
-        let title = "API did not store instructions!"
-        let message = "Spoonacular Food API does not have complete instructions. Please try again & select a different recipe!"
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "OK", style: .Default,handler:nil)
-        alert.addAction(action)
-        presentViewController(alert, animated: true, completion: nil)
-    }
+//    
+//    func alertNoInstructions(name: String) {
+//        let title = "API did not store instructions!"
+//        let message = "Spoonacular Food API does not have complete instructions. Please try again & select a different recipe!"
+//        
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+//        let action = UIAlertAction(title: "OK", style: .Default,handler:nil)
+//        alert.addAction(action)
+//        presentViewController(alert, animated: true, completion: nil)
+//    }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        // if recipe indexed has empty instructions array, ALERT, do not segue
-        let selected = allRecipes[indexPath.row]
-        let idInt = selected!["id"] as! Int
-        let id = String(idInt)
-        let directionsList:Array<String> = recipeInstance.getDirections(id)
-        if directionsList == [] {
-            alertNoInstructions(selected!["name"])
-        } else {
-            performSegueWithIdentifier("toDetailSegue", sender: indexPath)
-        }
+//        // if recipe indexed has empty instructions array, ALERT, do not segue
+//        let selected = allRecipes[indexPath.row]
+//        let idInt = selected!["id"] as! Int
+//        let id = String(idInt)
+//        let directionsList:Array<String> = recipeInstance.getDirections(id)
+//        if directionsList == [] {
+//            alertNoInstructions(selected!["name"])
+//        } else {
+//            performSegueWithIdentifier("toDetailSegue", sender: indexPath)
+//        }
+        performSegueWithIdentifier("toDetailSegue", sender: indexPath)
     }
     
     
